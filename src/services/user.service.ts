0681aaa07ab669a353fbb3b80c82ea71ecf5a617
danmoveo <dang@moveo.co.il>
2023-10-02 10:14:36 +0300

    adding user2

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,5 @@
 // src/users/user.service.ts
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto, LoginDto } from '../dto/user.dto';
@@ -8,23 +8,52 @@ import { User } from 'src/models/user.model';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel( User.name) private readonly userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
 
   async register(createUserDto: CreateUserDto): Promise<User> {
     const { email, password } = createUserDto;
 
-    // Hash the user's password
-    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+      // Check if the user with the same email already exists
+      const existingUser = await this.userModel.findOne({ email }).exec();
+      if (existingUser) {
+        throw new ConflictException('User with this email already exists');
+      }
 
-    const createdUser = new this.userModel({
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const createdUser = new this.userModel({
         email,
-      password: hashedPassword, // Store the hashed password in the database
-    });
+        password: hashedPassword, 
+      });
 
-    return createdUser.save();
+      return createdUser.save();
+    } catch (error) {
+      throw new Error('Unable to register user');
+    }
   }
 
   async login(loginDto: LoginDto): Promise<User | null> {
-    return this.userModel.findOne({ email: loginDto.email }).exec();
+    const { email, password } = loginDto;
+
+    try {
+      const user = await this.userModel.findOne({ email }).exec();
+
+      if (user) {
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (isPasswordValid) {
+          return user;
+        } else {
+          throw new UnauthorizedException('Invalid password');
+        }
+      } else {
+        throw new Error('User not found');
+      }
+    } catch (error) {
+      throw new Error('Unable to login');
+    }
   }
 }
